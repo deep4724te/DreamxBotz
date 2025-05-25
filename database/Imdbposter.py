25854df35b49232bc69b70a3d9ee1daa9b50e651
deep4724te <deepnew0.35@gmail.com>
2025-05-25 23:28:09 +0530

    Update Imdbposter.py

--- database/Imdbposter.py
+++ database/Imdbposter.py
@@ -122,7 +122,7 @@ async def get_movie_details(query, id=False, file=None):
             'poster_url': poster_url,
             'plot': plot,
             'rating': str(movie.get("rating")),
-            'url': f'https://catbox.moe/title/tt{movieid}'
+            'url': f'https://envs.sh/title/tt{movieid}'
         }
 
     except Exception as e:
